c7906f8eb9d0798c559a80e79a6a2de974ac860e
Skyl3r <kpereksta@gmail.com>
2013-10-13 13:44:53 -0400

    Moved SEActivity, added a renderer

---
+++ src/environment/SERenderer.java
@@ -0,0 +1,13 @@
+package environment;
+
+import android.content.Context;
+import rajawali.renderer.RajawaliRenderer;
+
+public class SERenderer extends RajawaliRenderer
+{
+
+	public SERenderer(Context context) {
+		super(context);
+	}
+
+}
